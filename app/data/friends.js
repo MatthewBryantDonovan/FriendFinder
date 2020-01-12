66681c92b07e1f416703068078782cafd3d2f144
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-01-11 22:17:27 -0500

    front end work to shape the user experience

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,77 +1,77 @@
 var allFriends = [{
-    "name":"Perfect ManA",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Simon",
+    "photo":"http://images2.fanpop.com/image/photos/13300000/Simon-Belmont-the-belmont-clan-castlevania-13324991-300-434.jpg",
     "scores":[
         1,
+        4,
+        3,
+        3,
+        5,
+        5,
         2,
+        5,
+        4,
+        2
+      ]
+  },{
+    "name":"Maria",
+    "photo":"https://i.pinimg.com/originals/22/a5/0a/22a50acbf97e7f7c6c378d0446fbeac1.jpg",
+    "scores":[
+        5,
+        1,
+        2,
+        4,
+        4,
+        1,
+        3,
+        5,
+        5,
+        3
+      ]
+  },{
+    "name":"Vlad Tepes",
+    "photo":"https://fsmedia.imgix.net/aa/a2/77/5b/1447/4adb/9aeb/e9394510f27d/dracula-in-netflixs-castlevania-animated-series.jpeg?auto=compress&h=1200&w=1200&crop=edges&fit=crop",
+    "scores":[
+        5,
+        4,
+        3,
+        2,
+        1,
+        4,
+        5,
+        3,
+        2,
+        1
+      ]
+  },{
+    "name":"Richter",
+    "photo":"https://vignette.wikia.nocookie.net/castlevania/images/b/bc/Richter_HOD.png/revision/latest?cb=20190930033701",
+    "scores":[
+        2,
+        4,
         3,
         4,
-        5,
-        6,
-        7,
-        8,
-        9,
-        10
-      ]
-  },{
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
         4,
         4,
-        5,
-        1,
         2,
-        5,
-        4,
-        1
-      ]
-  },{
-    "name":"Bahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },{
-    "name":"Cahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-      ]
-  },{
-    "name":"Perfect ManZ",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        1,
         2,
         3,
+        1
+      ]
+  },{
+    "name":"Shanoa",
+    "photo":"https://am22.akamaized.net/tms/cnt/uploads/2014/10/mi-coe-shanoa-307x480.jpg",
+    "scores":[
+        1,
+        3,
         4,
+        3,
         5,
-        6,
-        7,
-        8,
-        9,
-        10
+        3,
+        2,
+        1,
+        3,
+        3
       ]
   }
 ];
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -14,4 +14,8 @@ router.get("/survey", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
 });
 
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+
 module.exports = router;
